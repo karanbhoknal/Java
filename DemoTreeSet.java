08813dd4eda60a2ab878b24dd87b75d30e571709
karanbhoknal <148673737+karanbhoknal@users.noreply.github.com>
2024-03-01 09:15:53 +0530

    Add files via upload

---
+++ DemoTreeSet.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+ class DemoTreeSet 
+ { 
+ public static void main(String args[]) 
+ { 
+ TreeSet treeset1 = new TreeSet(); 
+ treeset1.add("Monday"); 
+ treeset1.add("Tuesday"); 
+ treeset1.add("Wednesday"); 
+ treeset1.add("Thursday"); 
+ treeset1.add("Friday"); 
+ treeset1.add("Saturday"); 
+ treeset1.add("Sunday"); 
+ System.out.println(treeset1); 
+ }
+ }
